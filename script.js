019dc5ac35d8defae3ba3d8b0ec3cd7a82a321b7
Kingsley Okafor <83710137+VanTyse@users.noreply.github.com>
2021-10-13 02:06:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ navItems.state = true;
 
 menu.addEventListener('click', () => {
     if(navItems.state){
-        navItems.style.display = 'block';
+//         navItems.style.display = 'block';
         navItems.style.right = '0';
         menu.style.display = 'none'
         closeButton.style.display = 'block';
@@ -15,7 +15,7 @@ menu.addEventListener('click', () => {
 
 closeButton.addEventListener('click', () => {
     if(!navItems.state){
-        navItems.style.display = 'none';
+//         navItems.style.display = 'none';
         navItems.style.right = '-120%'
         menu.style.display = 'block'
         closeButton.style.display = 'none';
