4a1e810c88b842fe975c2a278c357edcd812e790
Kingsley Okafor <83710137+VanTyse@users.noreply.github.com>
2021-10-19 23:19:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const closeButton = document.querySelector('.close-btn')
 const navItems = document.querySelector('.nav-items');
 navItems.state = true;
 
-window.addEventListener('click', function(event){
-    if(event.target.id != 'nav-items' && event.target.id != 'menu-bar'){
-        navItems.style.right = '-100%'
-        closeButton.style.display = 'none'
-        menu.style.display = 'block'
-        navItems.state = !navItems.state
-    }
+// window.addEventListener('click', function(event){
+//     if(event.target.id != 'nav-items' && event.target.id != 'menu-bar'){
+//         navItems.style.right = '-100%'
+//         closeButton.style.display = 'none'
+//         menu.style.display = 'block'
+//         navItems.state = !navItems.state
+//     }
+// })
+
+navItems.addEventListener('blur', () =>{
+    navItems.style.right = '-100%'
 })
 
 menu.addEventListener('click', () => {
