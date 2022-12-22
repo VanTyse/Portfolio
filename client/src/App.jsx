6f388eab76048f20eed7d517c167f87746aa9fa7
vantyse <nonsokingsley61@gmail.com>
2022-12-22 11:06:45 +0100

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,11 +27,11 @@ function Header () {
     return (
         <header className={`header ${direction === 'up' ? 'show-header' : 'hide-header'}`}>
             <nav className='nav-items'>
-                <NavItem item={'About Me'}/>
-                <NavItem item={'Experience'}/>
-                <NavItem item={'Projects'}/>
-                <NavItem item={'Skills'}/>
-                <NavItem item={'Education'}/>
+                <NavItem item={'about me'}/>
+                <NavItem item={'experience'}/>
+                <NavItem item={'projects'}/>
+                <NavItem item={'skills'}/>
+                <NavItem item={'education'}/>
             </nav>
         </header>
     )
@@ -39,7 +39,7 @@ function Header () {
 
 const NavItem = ({item, index}) => {
     return (
-        <div className="nav-item"> <a href={`#${item}`}>{item}</a></div>
+        <div className="nav-item"> <a href={`#${item === 'about me' ? 'about-me' : item}`}>{item}</a></div>
     )
 }
 
@@ -71,7 +71,7 @@ function AboutMeSection(){
     }, [staticRef.current])
 
     return(
-        <div ref={staticRef} className="about-me">
+        <div ref={staticRef} className="about-me" id='about-me'>
             <h4 className='sub-heading'><span>01.</span> About Me</h4>
             <div className='flex'>
                 <div className="right image-container">
@@ -84,12 +84,12 @@ function AboutMeSection(){
                 <div className="left">
                     <p className='intro'>
                         Hello! My name is Kingsley and I enjoy creating things that live on the internet. Such a nice place. 
-                        My interest in web development started back in 2019 when I wanted to wish my girlfriend a happy birthday <em>(in style)</em>.
+                        My interest in web development started back in 2019 when I wanted to wish a close friend 'happy birthday' <em>(in style)</em>.
                     </p>
                     <p className='intro'>
                         While I wouldn't do something so cheesy ever again, I have gone beyond that to building software that people
-                        would love using. I have little professional work experience so far but have worked on personal projects like designing the algorithm for chess, 
-                        making my own version of the <Link style={{textDecoration: 'none'}} to='/games/2048'><span>2048 game</span></Link> and even worked on building the UI for a startup as part of a team.
+                        would love using. I have some professional work experience, and I have worked on personal projects like developing a class management system for my class in University of Nigeria,
+                        making my own version of the <Link style={{textDecoration: 'none'}} to='/games/2048'><span>2048 game</span></Link> and even worked on building the application for a startup as part of a team.
                     </p>
                     <p className='intro'>
                         When I am not handling pest control on my laptop, I am writing in whatever way I feel is fun at the time.
@@ -118,14 +118,14 @@ function ExperienceSummary(){
         'Experience with Database Management Solutions.',
         'Knowledge of the complete MERN stack.',
         'Knowledge of UI/UX design principles.',
-        'Experience publishing web applications to Netlify and Heroku.',
+        'Experience publishing web applications  to Netlify, Heroku, AWS Amplify and Railway.',
         'Experience using version control tool–Git.',
-        'High level skills in building custom React UI components.',
-        'Acceptable level skills building RESTful APIs performing CRUD operations.'
+        'High level skills in building reusable, custom React UI components.',
+        'High level skills building RESTful APIs performing CRUD operations.'
     ]
 
     return(
-        <div ref={staticRef} className="experience-summary">
+        <div ref={staticRef} className="experience-summary" id='experience'>
             <h4 className="sub-heading"><span>02. </span>Experience Summary</h4>
             <h6>Proficient experience in creating user friendly and highly optimized web applications, especially at the ‘frontend’ of things.</h6>
             <ul>
@@ -149,16 +149,30 @@ function ProfessionalExperience(){
         }
     }, [staticRef.current])
 
+    useEffect(() => {
+        if (experienceId == 0) {
+            document.documentElement.style.setProperty('--experience-height', '400px')
+            document.documentElement.style.setProperty('--experience-height-sm', '600px')
+        }
+        else {
+            document.documentElement.style.setProperty('--experience-height', '250px')
+            document.documentElement.style.setProperty('--experience-height-sm', '350px')
+        }
+        console.log(experienceId);
+    }, [experienceId])
+
     let experiences =[
         {
             id : '0',
             companyName : 'Foodsub Africa',
             website : 'https://foodsub.africa',
             workDone : ['Led a team of react developers to work on responsive user interfaces for the company’s web applications. The company’s users use the mobile application to use the company’s services, while the web applications are used by the staff to manage everything, like a really huge dashboard.',
-                        'Was part of the backend team building robust and secure apis using node, express, websockets, and mongodb for the database.',
-                        'Worked with a DevOps engineer to host the project on AWS.'   
+                        'Built an admin system that increased tested efficiency by 30%.',
+                        'Built a service provider system that increased tested efficiency by about 67%.',
+                        'Built a completely secure and robust payment system that utilized the flutterwave, paystack and mono apis.',
+                        'Was part of the backend team building robust and secure apis using node, express, websockets, and mongodb for the database.'
                         ],
-            duration: 'July 2022 - August 2022',
+            duration: 'July 2022 - December 2022',
             role: 'Full Stack Developer - (Contract)'
         },
         {
@@ -241,7 +255,7 @@ function Projects(){
     }, [staticRef.current])
 
     return(
-        <div ref={staticRef} className="projects-container">
+        <div ref={staticRef} className="projects-container" id='projects'>
             <h4 className="sub-heading"><span>04. </span>Projects I've worked on</h4>
             <div className="projects container">
              {
@@ -273,7 +287,7 @@ function Project({projectName, desc, externalLink, githubLink, skills}){
                         <span className="external-link-icon"><a target='_blank' href={externalLink}><i class="bi bi-box-arrow-up-right"></i></a></span>
                     </div>
                 </div>
-                <h5 className='project-name'>{projectName}</h5>
+                <h5 className='project-name'><a href={externalLink} target='_blank'>{projectName}</a></h5>
                 <p>{desc}
                     <a href={externalLink} target='_blank'>here.</a>
                 </p>
@@ -298,10 +312,10 @@ function Skills(){
         }
     }, [staticRef.current])
 
-    const hardSkills = ['react', 'node', 'express', 'mongodb', 'redux', 'bootstrap', 'sass', 'next', 'agile development', 'javaScript', 'github', 'git']
+    const hardSkills = ['reactjs', 'nodejs', 'expressjs', 'mongodb', 'redux', 'bootstrap', 'sass', 'nextjs', 'agile development', 'javaScript', 'github', 'git']
     const softSkills = ['problem-solving', 'critical-thinking', 'time-management', 'listening', 'collaboration', 'written communication']
     return(
-        <div ref={staticRef} className="skills-container">
+        <div ref={staticRef} className="skills-container" id='skills'>
             <h4 className='sub-heading'><span>05. </span>Skills</h4>
             <div className='inner-container'>
                 <div className="tech-skills">
@@ -346,7 +360,7 @@ function Education(){
     }, [staticRef.current])
 
     return(
-        <div ref={staticRef} className="education">
+        <div ref={staticRef} className="education" id='education'>
             <h4 className="sub-heading"><span>06. </span>Education</h4>
             <ul>
                 <li>
@@ -368,8 +382,36 @@ export default Portfolio
 
 const projectsArray = [
     {
+        'projectName': 'Foodsub Africa',
+        'desc': 'This is the home page for the startup that I currently have a contract with. Sadly, this is the only stuff I can share. This website however proves my expertise as a react developer. Check it out ',
+        'externalLink' : 'https://www.foodsub.africa/',
+        'githubLink': null,
+        'skills': ['react', 'css']
+    },
+    {
+        'projectName': 'Class_MGT App',    
+        'desc': 'This is an application that focuses on helping university students better manage some of their class activities, like registering courses, getting notifications, access to list of courses, registration, etc. Check it out ',
+        'externalLink' : 'https://class-mgt.up.railway.app/',
+        'githubLink': null,
+        'skills': ['react', 'css', 'expressjs', 'socket.io']
+    },
+    {
+        'projectName': 'VanChat',
+        'desc': 'This is a chat application supporting duplex communication between two different users in real-time. Experience the world changing effect of two-way http comunication using websockets  ',
+        'externalLink' : 'https://vanchat.up.railway.app/',
+        'githubLink': 'https://github.com/VanTyse/van_chat',
+        'skills': ['react', 'css', 'expressjs', 'socket.io']
+    },
+    {
+        'projectName': 'Vantyse Docs',
+        'desc': 'My resume was typed with google docs and this project is a google docs clone focusing more on the backend. In this project users are able to make documents, edit documents, delete documents and even collaborate on documents in real time. Log in and create a document, then open up an icognito window or use a different browser to test the ability to collaborate with a differet user ',
+        'externalLink' : 'https://vantyse-docs.netlify.app/',
+        'githubLink': 'https://github.com/VanTyse/vantyse_docs',
+        'skills': ['react', 'css', 'expressjs', 'socket.io']
+    },  
+    {
         'projectName': 'Vantyse',
-        'desc': `This is a big personal project that displays my skills as a proficient MERN developer. This is a cross between a social media website, blog and game hub. It's a website where users can read content, play games and have personalized to-do lists. This portfolio page happens to rest on this domain, so use the nav at the top to check out the site `,
+        'desc': `This is a big personal project that displays my skills as a proficient MERN developer. This is a cross between a social media website, blog and game hub. It's a website where users can read content, play games and have personalized to-do lists. Check out the site `,
         'externalLink' : 'https://vantyse.com',
         'githubLink': 'https://github.com/VanTyse/Van_tyse',
         'skills': ['react', 'css', 'express', 'mongoose'],
@@ -388,26 +430,4 @@ const projectsArray = [
         'githubLink': 'https://github.com/VanTyse/Van_tyse/tree/main/frontend/src/routes/2048',
         'skills': ['react', 'css']
     },
-    {
-        'projectName': 'Foodsub Africa',
-        'desc': 'This is the home page for the startup that I currently have a contract with. I am currently building more stuff for the startup that I am not allowed to share. This website however proves my expertise as a react developer. Check it out ',
-        'externalLink' : 'https://www.foodsub.africa/',
-        'githubLink': null,
-        'skills': ['react', 'css']
-    },
-    {
-        'projectName': 'Vantyse Docs',
-        'desc': 'My resume was typed with google docs and this project is a google docs clone focusing more on the backend. In this project users are able to make documents, edit documents, delete documents and even collaborate on documents in real time. Log in and create a document, then open up an icognito window or use a different browser to test the ability to collaborate with a differet user ',
-        'externalLink' : 'https://vantyse-docs.netlify.app/',
-        'githubLink': 'https://github.com/VanTyse/vantyse_docs',
-        'skills': ['react', 'css', 'expressjs', 'socket.io']
-    },  
-    {
-        'projectName': 'Class_MGT App',
-        
-        'desc': 'This is an application tailored to managing the needs of student management in the classroom; complete with both backend and frontend.',
-        'externalLink' : 'https://class-mgt.up.railway.app/',
-        'githubLink': null,
-        'skills': ['react', 'css', 'expressjs', 'socket.io']
-    }
 ]
